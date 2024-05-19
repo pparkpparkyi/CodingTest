02115d98a9838f4f42f1d7dc0d8e4b7c675fa627
pparkpparkyi <165232941+pparkpparkyi@users.noreply.github.com>
2024-05-20 01:16:27 +0900

    [Bronze V] Title: A+B - 8, Time: 160 ms, Memory: 14752 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+                StringTokenizer st;
+                int T = Integer.parseInt(br.readLine());
+
+                for (int i = 0; i < T; i++) {
+                    //readline()은 한 줄을 읽는 메소드
+                    //한줄을 읽어서, 그 글자를 int 값으로 반환함.
+                    st = new StringTokenizer(br.readLine()); //다음줄로 넘김
+                    //파일에 작성하는 부분. 따로 다음줄로 넘어가지 않기 때문에 \n을 사용함.
+                    int a = Integer.parseInt(st.nextToken());
+                    int b = Integer.parseInt(st.nextToken());
+                    bw.write(String.format("Case #%d: %d + %d = %d\n", i+1,a,b,a+b));
+                    //bw.write(String.format("Case #%d: %d\n", i + 1, Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())));
+                    //위에처럼 할 수 있단다...
+                }
+                bw.flush(); // 버퍼에 남아있는 내용을 모두 출력한다. // 꼭 필요한과정
+                bw.close(); //파일을 닫는다.
+            }
+}
