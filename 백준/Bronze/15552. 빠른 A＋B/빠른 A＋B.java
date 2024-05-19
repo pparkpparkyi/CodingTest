a84e150280cdf70378299e9c63b8b81fc386f5a3
pparkpparkyi <165232941+pparkpparkyi@users.noreply.github.com>
2024-05-20 00:38:54 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 876 ms, Memory: 236320 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.StringTokenizer;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        //파일을 읽기 위한 선언
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //파일에 쓰기 위한 선언
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            //readline()은 한 줄을 읽는 메소드
+            //한줄을 읽어서, 그 글자를 int 값으로 반환함.
+            st = new StringTokenizer(br.readLine()); //다음줄로 넘김
+            //파일에 작성하는 부분. 따로 다음줄로 넘어가지 않기 때문에 \n을 사용함.
+            bw.write((Integer.parseInt(st.nextToken())+Integer.parseInt(st.nextToken())) + "\n");
+        }
+        bw.flush(); // 버퍼에 남아있는 내용을 모두 출력한다. // 꼭 필요한과정
+        bw.close(); //파일을 닫는다.
+
+    }
+}
